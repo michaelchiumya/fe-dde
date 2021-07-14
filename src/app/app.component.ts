0de4b00f4b44f79c7a076f97a21c41dd7db702f1
mj <michaelchiumya@yahoo.co.uk>
2021-07-14 18:26:08 +0100

    added menubar, carousel and routes to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
 import { Component } from '@angular/core';
+import { MegaMenuItem,MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'dde-fe';
-}
+  items: MenuItem[] = [];
+
+    ngOnInit() {
+      this.items = [
+        {
+          label:'Artists',
+          icon:'pi pi-fw pi-file',
+          items:[
+            {
+              label:'New',
+              icon:'pi pi-fw pi-plus',
+              items:[
+                {
+                  label:'Bookmark',
+                  icon:'pi pi-fw pi-bookmark'
+                },
+                {
+                  label:'Video',
+                  icon:'pi pi-fw pi-video'
+                },
+
+              ]
+            },
+
+            {
+              label:'Delete',
+              icon:'pi pi-fw pi-trash'
+            },
+            {
+              separator:true
+            },
+            {
+              label:'Export',
+              icon:'pi pi-fw pi-external-link'
+            }
+          ]
+        },
+        {
+          label:'Music',
+          icon:'pi pi-fw pi-users',
+          items:[
+            {
+              label:'Filter',
+              icon:'pi pi-fw pi-filter',
+              items:[
+                {
+                  label:'Print',
+                  icon:'pi pi-fw pi-print'
+                }
+              ]
+            },
+            {
+              icon:'pi pi-fw pi-bars',
+              label:'List'
+            }
+          ]
+        },
+        {
+          label:'Videos',
+          icon:'pi pi-fw pi-calendar',
+          items:[
+            {
+              label:'Edit',
+              icon:'pi pi-fw pi-pencil',
+              items:[
+                {
+                  label:'Save',
+                  icon:'pi pi-fw pi-calendar-plus'
+                },
+                {
+                  label:'Delete',
+                  icon:'pi pi-fw pi-calendar-minus'
+                },
+
+              ]
+            },
+            {
+              label:'Archieve',
+              icon:'pi pi-fw pi-calendar-times',
+              items:[
+                {
+                  label:'Remove',
+                  icon:'pi pi-fw pi-calendar-minus'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label:'Login',
+          icon:'pi pi-fw pi-power-off'
+        }
+      ]
+    }
+
+  }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MegaMenuModule} from 'primeng/megamenu';
+import {ButtonModule} from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import {CarouselModule} from 'primeng/carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MegaMenuModule,
+    MenubarModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
